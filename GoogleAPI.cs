3d9dfb8df6e0161a693ebe815a6c0a0db30d298f
Giannis Skoulis <giskou@gmail.com>
2009-09-25 16:04:18 +0000

    * GoogleAPI.cs: Created the class template GoogleResponse where the   json will be serialised

--- GoogleAPI.cs
+++ GoogleAPI.cs
@@ -16,9 +16,43 @@ namespace Monogle
 		private int page { get; set; }
 		private StringBuilder URL;
 		
-		public class ResponseData{
+		public class GooglePage{
 			
+			public string start;
+			public string label;
+		}
+		
+		public class GoogleCursor{
 			
+			public GooglePage[] pages;
+			public string estimatedResultCount;
+			public string currentPageIndex;
+			public string moreResultsUrl;
+		}
+		
+		public class GoogleSearchResult{
+			
+			public string GsearchResultClass;
+			public string unescapedUrl;
+			public string url;
+			public string visibleUrl;
+			public string cacheUrl;
+			public string title;
+			public string titleNoFormatting;
+			public string content;
+		}
+		
+		public class GoogleResponseData{
+			
+			public GoogleSearchResult[] results;
+			public GoogleCursor cursor;
+		}
+		
+		public class GoogleResponse{
+			
+			public GoogleResponseData responseData;
+			public string responseDetails;
+			public string responseStatus;
 		}
 		
 		public GoogleAPI(string v, string q, string rsz, string hl, string key, int start){
