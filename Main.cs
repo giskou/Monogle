e7da27326411805e357ac581bba4da9d314188a9
Giannis Skoulis <giskou@gmail.com>
2009-09-27 21:55:13 +0000

    * Monogle.pidb, Monogle.csproj:
    * gtk-gui/gui.stetic: fixed the stipid monogle namespace issue with
      monodevelop
    
    * Main.cs: removed monogle namespace 
    
    * Monogle.userprefs: dont know what changed here ...

--- Main.cs
+++ Main.cs
@@ -27,25 +27,22 @@
 using System;
 using Gtk;
 
-namespace Monogle{
+class MainClass	{
 	
-	class MainClass	{
+	public static void Main (string[] args){
 		
-		public static void Main (string[] args){
-			
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
-			
-			/*GoogleWebSearch testWebsearch = new GoogleWebSearch("lalaki", "large", "en", "1", "off", " ", "0");
-			GoogleAPI.GoogleResponse testResponce = testWebsearch.Search();
-			
-			foreach (GoogleAPI.GoogleSearchResult result in testResponce.responseData.results){
-				Console.Write(result.title + "\n");
-				Console.Write(result.content + "\n\n");
-			}*/
-			
-		}
+		Application.Init ();
+		MainWindow win = new MainWindow ();
+		win.Show ();
+		Application.Run ();
+		
+		/*GoogleWebSearch testWebsearch = new GoogleWebSearch("lalaki", "large", "en", "1", "off", " ", "0");
+		GoogleAPI.GoogleResponse testResponce = testWebsearch.Search();
+		
+		foreach (GoogleAPI.GoogleSearchResult result in testResponce.responseData.results){
+			Console.Write(result.title + "\n");
+			Console.Write(result.content + "\n\n");
+		}*/
+		
 	}
 }
